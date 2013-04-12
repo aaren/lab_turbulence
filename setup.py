e8063f3b420898c44dc1613d66a1afbeaa11d203
Aaron O'Leary <eeaol@leeds.ac.uk>
2013-04-12 16:16:36 +0100

    project structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description': 'Analysis of gravity currents measured with PIV',
+        'author': "Aaron O'Leary",
+        'url': 'http://github.com/aaren/lab_turbulence',
+        'download_url': 'http://github.com/aaren/lab_turbulence/download',
+        'author_email': 'eeaol@leeds.ac.uk',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['gc_turbulence'],
+        'scripts': [],
+        'name': 'gc_turbulence'
+        }
+
+setup(**config)
