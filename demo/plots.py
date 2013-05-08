f963e46571ca9f51d34dd7e12f5761e26cf06f9e
Aaron O'Leary <eeaol@leeds.ac.uk>
2013-05-08 16:33:10 +0100

    TODO: test case for plotting
    plot title and labels.

--- demo/plots.py
+++ demo/plots.py
@@ -1,7 +1,10 @@
 import os
 
 from gc_turbulence.gc_turbulence.turbulence import SingleLayer2dRun
+# from gc_turbulence import SingleLayer2dRun
 
+import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 
 
@@ -18,25 +21,59 @@ run_lims = {'3b4olxqo': (2200, 4400),
             '3bbn7639': (850, 2400),
             '3bc4rhc0': (1300, 3000)}
 
+# run for testing
+test_run = '3ban2y82'
+wd = os.path.join(w_dir, test_run)
+run_kwargs = {'data_dir':  wd,
+              'ffmt':      'img*csv',
+              'parallel':  True,
+              'limits':    run_lims[test_run]}
+r = SingleLayer2dRun(**run_kwargs)
+
 
 class PlotRun(object):
     def __init__(self, run):
         self.index = run
-        # wd = os.path.join(w_dir, run)
-        wd = w_dir + run + '/'
+        wd = os.path.join(w_dir, run)
         run_kwargs = {'data_dir':  wd,
                       'ffmt':      'img*csv',
                       'parallel':  True,
                       'limits':    run_lims[run]}
         self.r = SingleLayer2dRun(**run_kwargs)
 
-    def plot_histogram(self, save=True):
+    def plot_histogram_U(self, save=True):
         U = self.r.U
         uf = U.flatten()
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.hist(uf, bins=1000, range=(-7, 3))
-        fname = 'histogram_' + self.index + '.png'
+        title = 'Streamwise velocity distribution, run {run}'
+        ax.set_title(title.format(run=run))
+        ax.set_xlabel('Streamwise velocity, pixels')
+        ax.set_ylabel('frequency')
+        ax.set_yticks([])
+
+        fname = 'histogram_U_' + self.index + '.png'
+        fpath = os.path.join(plot_dir, fname)
+
+        if save:
+            fig.savefig(fpath)
+        elif not save:
+            return fig
+
+    def plot_histogram_W(self, save=True):
+        U = self.r.W
+        uf = U.flatten()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.hist(uf, bins=1000, range=(-7, 3))
+        title = 'Vertical velocity distribution, run {run}'
+        ax.set_title(title.format(run=run))
+        ax.set_xlabel('Vertical velocity, pixels')
+        ax.set_ylabel('frequency')
+        ax.set_yticks([])
+
+        fname = 'histogram_W_' + self.index + '.png'
         fpath = os.path.join(plot_dir, fname)
 
         if save:
@@ -49,6 +86,7 @@ class PlotRun(object):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.contourf(modu, 50)
+        ax.set_title('Mean speed')
         fname = 'average_velocity_' + self.index + '.png'
         fpath = os.path.join(plot_dir, fname)
         if save:
@@ -56,9 +94,68 @@ class PlotRun(object):
         elif not save:
             return fig
 
+    def plot_median_velocity(self, save=True):
+        u_mod = np.hypot(self.r.U, self.r.W)
+        u_mod_med = stats.nanmedian(u_mod, axis=2)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.contourf(u_mod_med, 50)
+        ax.set_title('Median speed')
+        fname = 'median_velocity_' + self.index + '.png'
+        fpath = os.path.join(plot_dir, fname)
+        if save:
+            fig.savefig(fpath)
+        elif not save:
+            return fig
+
+    def plot_power(self, save=True):
+        # fourier transform over the time axis
+        U_point = np.abs(self.r.U[15, 30, :])
+
+        fig = plt.figure()
+        ax_location = fig.add_subplot(311)
+        ax_speed = fig.add_subplot(312)
+        ax_power = fig.add_subplot(313)
+
+        ax_location.contourf(self.r.U[:, :, 500], 50)
+        ax_location.set_title('Overview')
+        ax_location.plot(30, 15, 'wo')
+
+        # ???
+        times = np.array(range(len(U_point))) * 0.01
+
+        ax_speed.plot(times, U_point)
+        ax_speed.set_title('Absolute streamwise velocity')
+        ax_speed.set_xlabel('Time (s)')
+        ax_speed.set_ylabel('Streamwise velocity (pixels)')
+
+        power_spectrum = np.abs(np.fft.fft(U_point)) ** 2
+        freqs = np.fft.fftfreq(len(U_point), d=0.01)
+
+        ax_power.plot(freqs, power_spectrum, 'r.')
+        ax_power.set_title('Power Spectrum')
+        ax_power.set_yscale('log')
+        ax_power.set_xlim(0, 50)
+        ax_power.set_xlabel('Frequency (Hz)')
+        ax_power.set_ylabel('Power')
+
+        fig.tight_layout()
+
+        fname = 'power_spectrum_' + self.index + '.png'
+        fpath = os.path.join(plot_dir, fname)
+        # TODO: can a decorator replace this functionality?
+        if save:
+            fig.savefig(fpath)
+        elif not save:
+            return fig
+
+
 if __name__ == '__main__':
     for run in runs:
         print "Plotting " + run + "..."
         pr = PlotRun(run)
-        pr.plot_histogram()
+        pr.plot_histogram_U()
+        pr.plot_histogram_W()
         pr.plot_average_velocity()
+        pr.plot_median_velocity()
+        pr.plot_power()
