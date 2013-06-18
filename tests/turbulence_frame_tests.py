924ec8c6cdd12e4c908c1fea975b0c4ca0e48bbe
Aaron O'Leary <aaron.oleary@gmail.com>
2013-06-19 00:20:40 +0100

    test import stereo

--- tests/turbulence_frame_tests.py
+++ tests/turbulence_frame_tests.py
@@ -1,13 +1,16 @@
 from nose.tools import *
 
 from ..gc_turbulence.turbulence import SingleLayer2dFrame
+from ..gc_turbulence.turbulence import SingleLayer3dFrame
 
 
 csv_file = 'tests/ex_data/data/img.3b4olxqo.000500.csv'
 txt_file = 'tests/ex_data/data/Export.3atnh4dp.000500.txt'
+stereo_file = 'tests/ex_data/data/stereo_test.3eodh6wx.000011.txt'
 
 csv_frame = SingleLayer2dFrame(csv_file)
 txt_frame = SingleLayer2dFrame(txt_file)
+stereo_frame = SingleLayer3dFrame(stereo_file)
 
 
 def test_header_csv():
@@ -46,3 +49,17 @@ def test_data_read_txt():
     assert_equal(z[0, 0], 0)
     assert_almost_equal(u[0, 0], 0.400000005960464)
     assert_almost_equal(w[0, 0], 0)
+
+def test_data_read_stereo():
+    x = stereo_frame.x
+    z = stereo_frame.z
+    u = stereo_frame.u
+    v = stereo_frame.v
+    w = stereo_frame.w
+
+    assert_equal(x[50, 50], 20.2136267196845)
+    assert_equal(z[50, 50], 39.0232063188143)
+
+    assert_almost_equal(u[50, 50], 0.00388681469485164)
+    assert_almost_equal(w[50, 50], -0.00251218792982399)
+    assert_almost_equal(v[50, 50], 0.00109881022945046)
