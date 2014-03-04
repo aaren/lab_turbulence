e8a2c2c358c16acf1a821abae6249e36d08ee108
Aaron O'Leary <eeaol@leeds.ac.uk>
2014-03-04 16:09:36 +0000

    sort out init

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from gc_turbulence import *
--- gc_turbulence/__init__.py
+++ gc_turbulence/__init__.py
@@ -1,4 +1,8 @@
 import turbulence
+import analysis
 import util
 
 from turbulence import SingleLayerFrame, SingleLayerRun, AnalysisRun
+from turbulence import ProcessedRun
+from turbulence import PreProcessor
+from analysis import DMD
