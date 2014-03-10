fd2d040d5d16965bbadf983457562dc5631b4fc2
Aaron O'Leary <eeaol@leeds.ac.uk>
2014-03-10 16:21:16 +0000

    useful defaults

--- gc_turbulence/__init__.py
+++ gc_turbulence/__init__.py
@@ -8,3 +8,6 @@ from turbulence import PreProcessor
 from analysis import DMD
 
 from commander import cli
+
+default_root = '/home/eeaol/lab/data/flume2/main_data/'
+default_cache = default_root + 'cache/'
