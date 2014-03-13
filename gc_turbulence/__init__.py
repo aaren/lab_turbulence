574bd9d5d66f6bda5e530facbc452fc0d08890e0
Aaron O'Leary <eeaol@leeds.ac.uk>
2014-03-13 10:12:42 +0000

    more useful things in init

--- gc_turbulence/__init__.py
+++ gc_turbulence/__init__.py
@@ -5,9 +5,11 @@ import util
 from turbulence import SingleLayerFrame, SingleLayerRun
 from turbulence import ProcessedRun
 from turbulence import PreProcessor
+from turbulence import Parameters
 from analysis import DMD
 
 from commander import cli
 
 default_root = '/home/eeaol/lab/data/flume2/main_data/'
 default_cache = default_root + 'cache/'
+default_processed = default_root + 'processed/'
