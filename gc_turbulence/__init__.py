eca0dac5d149ca48a385f98218997c708471b7dd
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-08-12 14:47:36 +0100

    refactor: runbase

--- gc_turbulence/__init__.py
+++ gc_turbulence/__init__.py
@@ -2,11 +2,14 @@ import turbulence
 import analysis
 import util
 
-from turbulence import SingleLayerFrame, SingleLayerRun
+from runbase import SingleLayerFrame, SingleLayerRun
+
 from turbulence import ProcessedRun
 from turbulence import WavelessRun
 from turbulence import PreProcessor
+
 from attributes import Parameters
+
 from analysis import DMD
 
 from commander import cli
