c54da2f3ddddf90e8195c82fdf47030da68833e4
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-08-13 15:50:10 +0100

    reorg attributes

--- gc_turbulence/attributes.py
+++ gc_turbulence/attributes.py
@@ -5,6 +5,99 @@ import numpy as np
 import config
 
 
+class Parameters(object):
+    """For a given run index, determine the type of run
+    (single layer / two layer) and load the appropriate
+    parameters.
+
+    An instance of this class is a function that returns
+    a (ordered) dictionary of the run parameters.
+    """
+    # TODO: this path should be in init or config or somewhere else
+    root = config.default_root
+    single_layer_parameters = config.single_layer_parameters
+    two_layer_parameters = config.two_layer_parameters
+
+    single_layer_headers = [('run_index',        '|S10'),
+                            ('H',                np.float),
+                            ('D',                np.float),
+                            ('L',                np.float),
+                            ('rho_ambient',      np.float),
+                            ('rho_lock',         np.float),
+                            ('T_ambient',        np.float),
+                            ('T_lock',           np.float),
+                            ('n_sample_ambient', np.float),
+                            ('n_sample_lock',    np.float),
+                            ('T_sample_ambient', np.float),
+                            ('T_sample_lock',    np.float),
+                            ]
+
+    two_layer_headers = [('run_index',      '|S10'),
+                         ('H',              np.float),
+                         ('D',              np.float),
+                         ('L',              np.float),
+                         ('h_1 / H',        np.float),
+                         ('rho_upper',      np.float),
+                         ('rho_lower',      np.float),
+                         ('rho_lock',       np.float),
+                         ('T_upper',        np.float),
+                         ('T_lower',        np.float),
+                         ('T_lock',         np.float),
+                         ('n_sample_upper', np.float),
+                         ('n_sample_lower', np.float),
+                         ('n_sample_lock',  np.float),
+                         ('T_sample_upper', np.float),
+                         ('T_sample_lower', np.float),
+                         ('T_sample_lock',  np.float),
+                         ]
+
+    def __init__(self, run_index=None):
+        self.init_parameters()
+
+    def __call__(self, run_index):
+        run_type = self.determine_run_type(run_index)
+        if run_type == 'single layer':
+            return self.get_run_info(self.single_layer, run_index)
+        elif run_type == 'two layer':
+            return self.get_run_info(self.two_layer, run_index)
+        else:
+            return None
+
+    def get_run_info(self, parameters, index):
+        """Return the info for a given run index as an
+        OrderedDict.
+        """
+        line = np.where(parameters['run_index'] == index)
+        info = parameters[line]
+        keys = info.dtype.names
+        values = info[0]
+        odict = OrderedDict(zip(keys, values))
+        odict['run type'] = self.determine_run_type(index)
+        return odict
+
+    def determine_run_type(self, run_index):
+        """Returns the run type as a string, either
+        'two_layer' or 'single_layer'.
+        """
+        if run_index in self.single_layer['run_index']:
+            return 'single layer'
+        elif run_index in self.two_layer['run_index']:
+            return 'two layer'
+        else:
+            return None
+
+    def init_parameters(self):
+        """Load the parameters files."""
+        self.single_layer = self.load_parameters(self.single_layer_parameters,
+                                                 self.single_layer_headers)
+        self.two_layer = self.load_parameters(self.two_layer_parameters,
+                                              self.two_layer_headers)
+
+    @staticmethod
+    def load_parameters(file, headers):
+        return np.loadtxt(file, dtype=headers, skiprows=2)
+
+
 class BaseAttributes(object):
     # data vertical step (m)
     dz = 0.00116
@@ -106,96 +199,3 @@ class ProcessedAttributes(BaseAttributes):
 
 class AnalysisAttributes(BaseAttributes):
     vectors = AnalysisVectors.vectors
-
-
-class Parameters(object):
-    """For a given run index, determine the type of run
-    (single layer / two layer) and load the appropriate
-    parameters.
-
-    An instance of this class is a function that returns
-    a (ordered) dictionary of the run parameters.
-    """
-    # TODO: this path should be in init or config or somewhere else
-    root = config.default_root
-    single_layer_parameters = config.single_layer_parameters
-    two_layer_parameters = config.two_layer_parameters
-
-    single_layer_headers = [('run_index',        '|S10'),
-                            ('H',                np.float),
-                            ('D',                np.float),
-                            ('L',                np.float),
-                            ('rho_ambient',      np.float),
-                            ('rho_lock',         np.float),
-                            ('T_ambient',        np.float),
-                            ('T_lock',           np.float),
-                            ('n_sample_ambient', np.float),
-                            ('n_sample_lock',    np.float),
-                            ('T_sample_ambient', np.float),
-                            ('T_sample_lock',    np.float),
-                            ]
-
-    two_layer_headers = [('run_index',      '|S10'),
-                         ('H',              np.float),
-                         ('D',              np.float),
-                         ('L',              np.float),
-                         ('h_1 / H',        np.float),
-                         ('rho_upper',      np.float),
-                         ('rho_lower',      np.float),
-                         ('rho_lock',       np.float),
-                         ('T_upper',        np.float),
-                         ('T_lower',        np.float),
-                         ('T_lock',         np.float),
-                         ('n_sample_upper', np.float),
-                         ('n_sample_lower', np.float),
-                         ('n_sample_lock',  np.float),
-                         ('T_sample_upper', np.float),
-                         ('T_sample_lower', np.float),
-                         ('T_sample_lock',  np.float),
-                         ]
-
-    def __init__(self, run_index=None):
-        self.init_parameters()
-
-    def __call__(self, run_index):
-        run_type = self.determine_run_type(run_index)
-        if run_type == 'single layer':
-            return self.get_run_info(self.single_layer, run_index)
-        elif run_type == 'two layer':
-            return self.get_run_info(self.two_layer, run_index)
-        else:
-            return None
-
-    def get_run_info(self, parameters, index):
-        """Return the info for a given run index as an
-        OrderedDict.
-        """
-        line = np.where(parameters['run_index'] == index)
-        info = parameters[line]
-        keys = info.dtype.names
-        values = info[0]
-        odict = OrderedDict(zip(keys, values))
-        odict['run type'] = self.determine_run_type(index)
-        return odict
-
-    def determine_run_type(self, run_index):
-        """Returns the run type as a string, either
-        'two_layer' or 'single_layer'.
-        """
-        if run_index in self.single_layer['run_index']:
-            return 'single layer'
-        elif run_index in self.two_layer['run_index']:
-            return 'two layer'
-        else:
-            return None
-
-    def init_parameters(self):
-        """Load the parameters files."""
-        self.single_layer = self.load_parameters(self.single_layer_parameters,
-                                                 self.single_layer_headers)
-        self.two_layer = self.load_parameters(self.two_layer_parameters,
-                                              self.two_layer_headers)
-
-    @staticmethod
-    def load_parameters(file, headers):
-        return np.loadtxt(file, dtype=headers, skiprows=2)
